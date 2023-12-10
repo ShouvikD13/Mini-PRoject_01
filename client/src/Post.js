103ec48dc39d0970749ce86454d30fc0334fa48f
ShouvikD13 <deyshouvik499@gmail.com>
2023-12-10 14:28:50 +0530

    Running condition - front end design, logout and blof image issues to be resolved

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,25 @@
-export default function Post(){
-    return(
-        <div className="post">
-        <div className="image">
-        <img src ="https://images.nationalgeographic.org/image/upload/v1639146339/EducationHub/photos/pillars-of-creation.jpg" alt="pillars of creation"></img>
-        </div>
-        <div className="texts">
-        <h2>Pillars of Creation</h2>
-        <p className="info">
-          <a className="author">Shouvik Dey</a>
-          <time>2023-12-09 15:32</time>
-        </p>
-        <p className="summary">Pillars of Creation is a photograph taken by the Hubble Space Telescope of elephant trunks of interstellar gas and dust in the Eagle Nebula, in the Serpens constellation, some 6,500–7,000 light-years (2,000–2,100 pc; 61–66 Em) from Earth.[1] These elephant trunks had been discovered by John Charles Duncan in 1920 on a plate made with the Mount Wilson Observatory 60-inch telescope.[2][3] They are named so because the gas and dust are in the process of creating new stars, while also being eroded by the light from nearby stars that have recently formed.[4]</p>
-        </div>
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
+
+export default function Post({_id,title,summary,cover,content,createdAt,author}) {
+
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </Link>
       </div>
-    );
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+        <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
